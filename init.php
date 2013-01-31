f75fae460427ab74a547ba18dc1d9ebbec3b3139
Yusuke Ando <yandod@gmail.com>
2013-01-31 14:10:28 +0900

    Update init.php

--- init.php
+++ init.php
@@ -1,4 +1,4 @@
 <?php
 $pluginContainer = ClassRegistry::getObject('PluginContainer');
 $pluginContainer->installed('cc_lolipop','0.1');
-putenv('HTTP_X_FORWARDED_HOST=');
+$_ENV['HTTP_X_FORWARDED_HOST'] = null;
